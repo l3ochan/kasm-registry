409c62455bb32d374e2f385f11975a8a056c4dbf
Léochan <81470534+l3ochan@users.noreply.github.com>
2025-07-03 23:50:38 +0200

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
   env: {
     name: 'Nekocorp',
     description: 'Custom kasm worspaces for Nekocorp',
-    icon: 'https://github.com/l3ochan/kasm-registry/blob/d384afc5789677f8b705c10d663d83f369691439/site/public/logo%20round.png',
+    icon: 'https://github.com/l3ochan/kasm-registry/blob/1.1/site/public/logo%20round.png',
     listUrl: 'https://l3ochan.github.io/kasm-registry/1.1/',
     contactUrl: 'https://github.com/l3ochan/nekocorp-workspace-kasm/issues',
   },
